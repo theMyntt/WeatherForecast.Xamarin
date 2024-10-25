774ae1f8f02b43b3f20bb59bf69f66ef41147911
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 00:26:54 -0300

    chore: using INotifyPropertyChanged

--- WeatherForecast.Xamarin/WeatherForecast.Xamarin/ViewModels/LocationViewModel.cs
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/ViewModels/LocationViewModel.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
 using WeatherForecast.Xamarin.Models;
 using WeatherForecast.Xamarin.Services;
 using Xamarin.Forms;
 
 namespace WeatherForecast.Xamarin.ViewModels
 {
-    public partial class LocationViewModel : ObservableObject
+    public partial class LocationViewModel : INotifyPropertyChanged 
 	{
-		[ObservableProperty]
 		private ObservableCollection<LocationDTO> locations;
-
-		[ObservableProperty]
 		private string query;
+
+		public ObservableCollection<LocationDTO> Locations
+		{
+			get => locations;
+			set => SetProperty(ref locations, value);
+		}
+
+		public string Query
+		{
+			get => query;
+			set => SetProperty(ref query, value);
+		}
 
 		private readonly LocationService _service;
 
-		public ICommand GetSpecific { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand GetSpecific { get; }
 		public ICommand Clear { get; }
 
 		public LocationViewModel()
@@ -53,6 +66,21 @@ namespace WeatherForecast.Xamarin.ViewModels
 		{
 			await Shell.Current.GoToAsync($"///Forecast?CityCode={cityCode}");
 		}
-	}
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
 }
 
