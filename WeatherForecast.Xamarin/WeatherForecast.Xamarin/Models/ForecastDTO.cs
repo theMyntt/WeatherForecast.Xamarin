8989b901a891683baa9fc293f30d7cdb1c5dc3e8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 00:51:09 -0300

    chore: forecast and weather dto

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/Models/ForecastDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherForecast.Xamarin.Models
+{
+    public struct ForecastDTO
+	{
+		public string Cidade { get; set; }
+		public string Estado { get; set; }
+		public string Atualizado_em { get; set; }
+		IEnumerable<WeatherDTO> Clima { get; set; }
+	}
+
+	public struct WeatherDTO
+	{
+		public string Data { get; set; }
+		public string Codicao { get; set; }
+		public string Condicao_desc { get; set; }
+		public int Min { get; set; }
+		public int Max { get; set; }
+		public int Indice_iv { get; set; }
+	}
+}
+
