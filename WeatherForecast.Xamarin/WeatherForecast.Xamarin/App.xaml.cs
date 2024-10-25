d0c0abdaf812476d2644ce54152dd58b1a6515ff
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-24 23:34:58 -0300

    chore: using shell

--- WeatherForecast.Xamarin/WeatherForecast.Xamarin/App.xaml.cs
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace WeatherForecast.Xamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart ()
