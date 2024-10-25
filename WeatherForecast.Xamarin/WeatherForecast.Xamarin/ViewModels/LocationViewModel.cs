1b7c05cd025b36126cb0daf98ad2593edcc4750f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 00:13:05 -0300

    chore: location view model

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/ViewModels/LocationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using WeatherForecast.Xamarin.Models;
+using WeatherForecast.Xamarin.Services;
+using Xamarin.Forms;
+
+namespace WeatherForecast.Xamarin.ViewModels
+{
+    public partial class LocationViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		private ObservableCollection<LocationDTO>? locations;
+
+		[ObservableProperty]
+		private string? query;
+
+		private readonly LocationService _service;
+
+		public ICommand GetSpecific { get; }
+		public ICommand Clear { get; }
+
+		public LocationViewModel()
+		{
+			_service = new();
+
+			GetSpecific = new Command(GetSpecificLocation);
+			Clear = new Command(ClearFilters);
+		}
+
+		public async void GetLocations()
+		{
+			Locations = await _service.PerformFindAll();
+		}
+
+		public async void GetSpecificLocation()
+		{
+			if (string.IsNullOrEmpty(Query))
+			{
+				Locations = await _service.PerformFindAll();
+				return;
+			}
+			Locations = await _service.PerformFindSpecific(Query);
+		}
+
+		public async void ClearFilters()
+		{
+			Query = "";
+			Locations = await _service.PerformFindAll();
+		}
+
+		public async void NavigateToForecast(int cityCode)
+		{
+			await Shell.Current.GoToAsync($"///Forecast?CityCode={cityCode}");
+		}
+	}
+}
+
