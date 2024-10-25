88120b64e49d060f55e206404ca6ddc924cebad8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-24 23:42:35 -0300

    chore: location dto

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/Models/LocationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WeatherForecast.Xamarin.Models
+{
+	public struct LocationDTO
+	{
+		public string Nome { get; set; }
+		public int Id { get; set; }
+		public string Estado { get; set; }
+	}
+}
+
