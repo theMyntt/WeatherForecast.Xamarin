f7ca503354b347725815090ac1c616fddf470a5f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 01:03:56 -0300

    chore: forecast view model

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/ViewModels/ForecastViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WeatherForecast.Xamarin.Models;
+using WeatherForecast.Xamarin.Services;
+
+namespace WeatherForecast.Xamarin.ViewModels
+{
+	public class ForecastViewModel : INotifyPropertyChanged
+	{
+        private ForecastDTO _location;
+        private int _cityCode;
+
+        public ForecastDTO Location
+        {
+            get => _location;
+            set => SetProperty(ref _location, value);
+        }
+
+        public int CityCode
+        {
+            get => _cityCode;
+            set => SetProperty(ref _cityCode, value);
+        }
+
+        private readonly ForecastService _service;
+
+		public ForecastViewModel()
+		{
+            _service = new ForecastService();
+		}
+
+        public async void GetLocationForecast()
+        {
+            Location = await _service.Perform(CityCode);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
+
