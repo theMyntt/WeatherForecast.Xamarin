1aece0dc9e343c51f0c66421a8e1c259520c06ae
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 01:19:53 -0300

    feat: forecast view

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/Views/ForecastView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using WeatherForecast.Xamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace WeatherForecast.Xamarin.Views
+{
+	[QueryProperty("CityCode", "CityCode")]
+	public partial class ForecastView : ContentPage
+	{
+		private readonly ForecastViewModel _viewModel;
+
+		public string CityCode { get; set; }
+
+		public ForecastView ()
+		{
+			InitializeComponent();
+			_viewModel = new ForecastViewModel();
+            BindingContext = _viewModel;
+		}
+
+        protected override async void OnAppearing()
+        {
+            if (!string.IsNullOrEmpty(CityCode))
+            {
+                _viewModel.CityCode = int.Parse(CityCode);
+            }
+            await _viewModel.GetLocationForecast();
+
+            base.OnAppearing();
+        }
+    }
+}
+
