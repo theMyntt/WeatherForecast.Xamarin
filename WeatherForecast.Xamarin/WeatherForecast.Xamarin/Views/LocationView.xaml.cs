4e0f87c88ed41ee755346ff67a384b06ec2bcfa5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-25 00:35:22 -0300

    feat: location view

---
+++ WeatherForecast.Xamarin/WeatherForecast.Xamarin/Views/LocationView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WeatherForecast.Xamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace WeatherForecast.Xamarin.Views
+{	
+	public partial class LocationView : ContentPage
+	{	
+		public LocationView ()
+		{
+			InitializeComponent();
+			BindingContext = new LocationViewModel();
+		}
+	}
+}
+
